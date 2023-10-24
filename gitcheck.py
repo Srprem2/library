642703348f68a0ae0a49f945dbe8ace36cd2379b
chandan sah <cshah5710@gmail.com>
2023-10-25 01:12:19 +0530

    Added checkEven def

--- gitcheck.py
+++ gitcheck.py
@@ -1,2 +1,7 @@
 a = input("Enter the change you have made : ")
 print(f'The change i made is given as : {a}')
+def checkEven(n):
+    if n%2 ==0:
+        return True
+n = int(input("Enter the number : "))
+print(checkEven(n))
